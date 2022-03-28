e0903de589e31868ac4cca35ec6b7d724338d6ac
ZonalDean <huber.deanc@gmail.com>
2022-03-28 23:11:21 +0700

    08 Constructor Function done

---
+++ 08-Constructor-function/02/main.js
@@ -0,0 +1,16 @@
+function Accumulator(startingValue) {
+    this.currentValue = startingValue;
+    this.y = 0;
+    this.read = function() {
+        this.currentValue = prompt("put in a number")
+        return Number(this.currentValue) + Number(this.y)
+    };
+    this.show = function() {
+        alert(this.currentValue);
+    };
+};
+
+let calculator = new Accumulator(5);
+
+calculator.read();
+calculator.show();
